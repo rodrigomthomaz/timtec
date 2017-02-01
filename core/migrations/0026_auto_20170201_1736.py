0fb816b9480b40b9c64aebbb53f3446e9a21a27f
Moacir Moda <moa.moda@gmail.com>
2017-02-01 17:51:36 -0200

    making migration that fix students that not enrolled in classes, institutotim/timtec#86

---
+++ core/migrations/0026_auto_20170201_1736.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def enroll_user_to_clas(apps, schema_editor):
+    """Enroll user to class."""
+    CourseStudent = apps.get_model("core", "CourseStudent")
+    for cs in CourseStudent.objects.all():
+
+        user_in_class = False
+        for class_ in cs.course.class_set.all():
+            if class_.students.filter(id=cs.user.id).exists():
+                user_in_class = True
+                break
+
+        if not user_in_class:
+            cs.course.default_class.students.add(cs.user)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0025_course_welcome_email'),
+    ]
+
+    operations = [
+        migrations.RunPython(enroll_user_to_clas),
+    ]
