8e700034da21d7d1a7ea7163abcf27a4dbf8ace7
Moacir Moda <moa.moda@gmail.com>
2017-02-19 10:04:04 -0300

    adding a complete version from tinyMCE, institutotim/timtec#65

--- administration/static/app/controllers/admin.lesson.controllers.js
+++ administration/static/app/controllers/admin.lesson.controllers.js
@@ -29,7 +29,14 @@
                 statusbar: false,
 
                 plugins: 'textcolor link',
-                toolbar: "undo redo styleselect bold italic forecolor backcolor link",
+                plugins: [
+                    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                    'searchreplace wordcount visualblocks visualchars code fullscreen',
+                    'insertdatetime media nonbreaking save table contextmenu directionality',
+                    'emoticons template paste textcolor colorpicker textpattern imagetools codesample'
+                ],
+                toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
             };
 
             // load youtube
