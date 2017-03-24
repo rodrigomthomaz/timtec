26c63c80c3cb153556ca8a4bb78e2d1125f5ed6b
Moacir Moda <moa.moda@gmail.com>
2017-03-24 11:34:42 -0300

    refactoring migration 0026 to not overclock memory RAM

--- core/migrations/0026_auto_20170201_1736.py
+++ core/migrations/0026_auto_20170201_1736.py
@@ -6,18 +6,20 @@ from django.db import migrations
 
 def enroll_user_to_clas(apps, schema_editor):
     """Enroll user to class."""
-    return
+
     CourseStudent = apps.get_model("core", "CourseStudent")
-    for cs in CourseStudent.objects.all():
 
-        user_in_class = False
-        for class_ in cs.course.class_set.all():
-            if class_.students.filter(id=cs.user.id).exists():
-                user_in_class = True
-                break
+    total_registers = CourseStudent.objects.all().count()
+    for count in range(0, total_registers, 1000):
+        for cs in CourseStudent.objects.all()[count:count+1000]:
+            user_in_class = False
+            for class_ in cs.course.class_set.all():
+                if class_.students.filter(id=cs.user.id).exists():
+                    user_in_class = True
+                    break
 
-        if not user_in_class:
-            cs.course.default_class.students.add(cs.user)
+            if not user_in_class:
+                cs.course.default_class.students.add(cs.user)
 
 
 class Migration(migrations.Migration):
