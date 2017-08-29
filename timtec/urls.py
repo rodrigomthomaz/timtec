10a33e1d07530bc205cff5300f2a13e8f9af3477
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2017-08-29 17:30:58 -0300

    Upgrade and support for latest django-debug-toolbar

--- timtec/urls.py
+++ timtec/urls.py
@@ -213,3 +213,11 @@ if settings.DEBUG:
 
 if 'ifs' in settings.INSTALLED_APPS:
     urlpatterns += (url(r'^ifs/', include('ifs.urls')),)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += patterns(
+        '',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
