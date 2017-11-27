271837e70a7f6623569039acfcbf3ddb1d69b263
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2017-11-27 14:33:25 -0200

    Save integration data

--- integration/utils.py
+++ integration/utils.py
@@ -1,9 +1,9 @@
-import json
-
 import requests
 import jwt
 
-from timtec import settings
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from .models import IntegrationData
 
 
 class IntegrationUtils():
@@ -15,3 +15,44 @@ class IntegrationUtils():
 
         r = requests.get(settings.INTEGRATION_PROFILE_URL, headers={'Authorization': 'Bearer ' + jwtToken})
         return r.json()
+
+
+def load_profile_all():
+
+    if not hasattr(settings, 'INTEGRATION_PROFILE_ALL_EDITIONS') or not settings.INTEGRATION_PROFILE_ALL_EDITIONS:
+        return
+
+    for edition in settings.INTEGRATION_PROFILE_ALL_EDITIONS:
+
+        params = {
+            'edition_id': edition,
+            'key': settings.INTEGRATION_PROFILE_ALL_SECRET
+        }
+
+        r = requests.get(settings.INTEGRATION_PROFILE_ALL_URL, params=params)
+        user_model = get_user_model()
+        if r.ok:
+            for profile in r.json().get('profiles'):
+                try:
+                    user = user_model.objects.get(id=profile.get('sub_id'))
+                except user_model.DoesNotExist:
+                    continue
+
+                save_integration_data(user, profile)
+
+
+def save_integration_data(user, json):
+
+    if not hasattr(user, 'integration_data'):
+        user.integration_data = IntegrationData()
+
+    user.integration_data.city = json.get('city')
+    user.integration_data.edition = json.get('edition')
+    user.integration_data.formation_type = json.get('formation_type')
+    user.integration_data.participation_type = json.get('participation_type')
+    user.integration_data.region = json.get('region')
+    user.integration_data.rf = json.get('rf')
+    user.integration_data.school = json.get('school')
+    user.integration_data.track = json.get('track')
+
+    user.integration_data.save()
